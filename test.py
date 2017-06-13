43f5a9078623eed76afedbde8aa996958d6ffc1e
lucashc <lucascrijns@gmail.com>
2017-06-13 20:15:26 +0200

    Cool test.py

--- test.py
+++ test.py
@@ -1,22 +1,28 @@
 from p5 import *
 from random import randint
-
+import math
 
 def setup():
     CreateWindow(640, 480)
     background(name="white")
-    global r
-    r = 0
+    global deg
+    deg = 0
+
+y, x, z = 0, 0, 1
 
 def draw():
-    global r
-    # print(r)
+    clear()
     fill(name="red")
-    translate(100, 100)
-    rotate(r)
-    rect(0, 0, 50, 50)
+    global y, x
+    translate(300, 300)
+    rotate(math.sin(x))
+    rect(math.sin(y)*50, math.cos(x)*50, 50, 50)
+    y += 0.1; x += 0.1
+    translate(100, math.cos(y)*20+100)
+    rotate(math.sin(x))
+    triangle(0,0,100, 0, 50, 50)
+
 
 def KeyPressed():
-    global r
-    r += 1
-    print(r)
+    pass
+
