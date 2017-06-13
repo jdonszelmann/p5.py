904e068a7b290b908421fdfe31837816deb23cb3
lucashc <lucascrijns@gmail.com>
2017-06-13 20:36:13 +0200

    Cool test.py

--- test.py
+++ test.py
@@ -13,13 +13,14 @@ y, x, z = 0, 0, 1
 def draw():
     clear()
     fill(name="red")
-    global y, x
+    global y, x, z
     translate(300, 300)
     rotate(math.sin(x))
-    rect(math.sin(y)*50, math.cos(x)*50, 50, 50)
-    y += 0.1; x += 0.1
-    translate(100, math.cos(y)*20+100)
-    rotate(math.sin(x))
+    rect(math.sin(y)*50, math.cos(x)*50, 50* (abs(math.cos(y)) + 0.5), 50* (abs(math.sin(x)) + 0.5))
+    y += 0.1; x += 0.1; z += 0.03
+    translate(100, math.cos(y)*40+100)
+    rotate(math.sin(x)*0.5)
+    fill(Color(abs(math.cos(z))*255, abs(math.sin(z))*255, abs(math.cos(z))*255))
     triangle(0,0,100, 0, 50, 50)
 
 
