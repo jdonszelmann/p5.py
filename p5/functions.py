e426819648fad75f61582504744d238376862277
lucashc <lucascrijns@gmail.com>
2017-06-14 22:07:43 +0200

    Added ellips function

--- p5/functions.py
+++ p5/functions.py
@@ -4,6 +4,7 @@ from .classes import Color
 from p5.classes import Vector
 from p5.core import *
 from .color import Colors
+import math
 
 def _get_coords(x, y, coord1,coord2):
     v = Vector(coord1,coord2)
@@ -37,8 +38,8 @@ def rect(x, y, w, h):
         x -= w
         y -= h
         w *= 2
-        h *= 2 
- 
+        h *= 2
+
     points = _get_coords(originalx,originaly,x, y) + _get_coords(originalx,originaly,x + w, y) + _get_coords(originalx,originaly,x + w, y + h) + _get_coords(originalx,originaly,x, y + h)
     Globals.WINDOWMANAGER.selectedwindow.batch.add(4, pyglet.gl.GL_QUADS, graphstyle("DAN", Globals.WINDOWMANAGER.selectedwindow.drawsettings.strokeweight),
                                                    ('v2f', points),
@@ -49,7 +50,7 @@ def line(x1,y1,x2,y2):
     Globals.WINDOWMANAGER.selectedwindow.batch.add(2, pyglet.gl.GL_LINES, graphstyle("DAN", Globals.WINDOWMANAGER.selectedwindow.drawsettings.strokeweight),
                                                ('v2f', points),
                                                ('c4f', 2 * Globals.WINDOWMANAGER.selectedwindow.drawsettings.stroke.get(True))
-                                               )    
+                                               )
 
 def triangle(x1, y1, x2, y2, x3, y3):
     line_color = Globals.WINDOWMANAGER.selectedwindow.drawsettings.strokecolor.get()
@@ -58,6 +59,22 @@ def triangle(x1, y1, x2, y2, x3, y3):
                                                    ('v2f', points),
                                                    ('c4f', 3 * Globals.WINDOWMANAGER.selectedwindow.drawsettings.fillcolor.get(True))
                                                    )
+
+def ellipse(x, y, w, h):
+    ACCURACY = 500
+    global ellipse_drawing
+    start = (x, y)
+    r_cos = 0.5*w
+    r_sin = 0.5*h
+    points = []
+    for i in range(ACCURACY):
+        points.append(_get_coords(start[0] + math.cos(i/ACCURACY*2*math.pi)*r_cos, start[1] + math.sin(i/ACCURACY*2*math.pi)*r_sin))
+    points = [i for sub in points for i in sub]
+    Globals.WINDOWMANAGER.selectedwindow.batch.add(len(points)//2, pyglet.gl.GL_POLYGON, graphstyle("DA", Globals.WINDOWMANAGER.selectedwindow.drawsettings.strokeweight),
+                                                   ('v2f', points),
+                                                   ('c4f', len(points)//2 * Globals.WINDOWMANAGER.selectedwindow.drawsettings.fillcolor.get(True))
+                                                   )
+
 # def test():
 #     class CustomGroup1(pyglet.graphics.Group):
 #         def set_state(self):
@@ -147,7 +164,7 @@ def stroke(*args, **kwargs):
             Globals.WINDOWMANAGER.selectedwindow.drawsettings.stroke = value
             return
     Globals.WINDOWMANAGER.selectedwindow.drawsettings.stroke = Color(*args, **kwargs)
-    
+
 def strokeweight(weight:int = 1):
     Globals.WINDOWMANAGER.selectedwindow.drawsettings.strokeweight = weight
 
--- test.py
+++ test.py
@@ -7,19 +7,23 @@ def setup():
     background(name="white")
     caption("hey!")
     fullscreen()
-    minsize(600,400)
-    maxsize(1200,800)
 
-r = 0
+col = 0
+x,y = 0,0
+n = True
 
 def draw():
-    global r
-    background()
-    r += 0.01
+    if n: clear()
+    global col, x, y
+    fill(math.cos(0.2*col)*255, math.sin(0.2*col)*255, math.tan(0.2*col)*255)
+    translate(math.cos(x)*100+200+math.sin(y-0.2)*25, math.atan(y)*100 + 100 + math.sin(x)*50+math.cos(y+3)*50)
+    rotate(math.cos(y)*TAU)
+    ellipse(0, 0, abs(math.cos(y)*50)+20, abs(math.sin(x)*30)+20)
+    x += 0.05; y += 0.05; col += 0.1
 
     strokeweight(1)
     line(0,0,100,100)
-    rotate(r)
+    rotate(col)
     rectmode("CENTER")
     fill(255)
     translate(200,200)
@@ -29,4 +33,6 @@ def draw():
     text("hi",100,100)
 
 def KeyPressed():
-    pass
+    global n
+    if n: n = False
+    else: n = True
